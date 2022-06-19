a6c64360df5d6043af3f4df75ba83ba3a8e42c74
Julie Webb <julief.webb@gmail.com>
2022-06-19 18:41:39 -0400

    adds map display from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+
+// import packages
+import './App.css';
+
+
 import Header from './Components/Header/Header'; 
 import Form from './Components/Form/Form';
 import Map from './Components/Map/Map';
 import ResultsPage from './Pages/ResultsPage';
 import AddressPage from './Pages/AddressPage';
 import Footer from './Components/Footer/Footer';
-
-import './App.css';
 
 function App() {
   return (
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,4 +1,46 @@
+
+import {useEffect, useState, useRef} from 'react';
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+import * as tt from '@tomtom-international/web-sdk-maps';
+
+const MAX_ZOOM = 17;
+
 const Map = (props) => {
-    return <h1>This is the Map</h1>
+    const mapElement = useRef();
+    const [mapLongitude, setMapLongitude] = useState(-121.91599);
+    const [mapLatitude, setMapLatitude] = useState(37.36765);
+    const [mapZoom, setMapZoom] = useState(13);
+    const [map, setMap] = useState({});
+
+    const increaseZoom = () => {
+        if (mapZoom < MAX_ZOOM) {
+            setMapZoom(mapZoom + 1);
+        }
+    };
+        
+    const decreaseZoom = () => {
+        if (mapZoom > 1) {
+            setMapZoom(mapZoom - 1);
+        }
+    };
+    
+    const updateMap = () => {
+        map.setCenter([parseFloat(mapLongitude), parseFloat(mapLatitude)]);
+        map.setZoom(mapZoom);
+    };
+
+    useEffect(() => {
+    let map = tt.map({
+        key: 'KXYIOAheM7cRQpB5GosJco3nGKGWSYg3',
+        container: mapElement.current,
+        center: [mapLongitude, mapLatitude],
+        zoom: mapZoom
+    });
+    setMap(map);
+    return () => map.remove();
+    }, []);
+
+
+    return <div ref={mapElement} className="mapDiv"></div>
 }; 
 export default Map; 
