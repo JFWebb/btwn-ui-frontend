3d9b1e50dbcfa3abc7b60392308093440afeccf2
Annie Ma <anniema@Annies-MacBook-Pro.local>
2022-06-23 15:47:47 -0400

    new css title

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,25 @@
-import {login, logout} from '../../services/firebase';
-import {Link} from 'react-router-dom';
-import './Header.styles.css'; 
+import { login, logout } from '../../services/firebase';
+import { Link } from 'react-router-dom';
+import './Header.styles.css';
 
 
 const Header = (props) => {
 
 
     return (
-        <nav className = 'nav-top'>
-            <Link to ="/">
-                <div className='title'> BTWN U+I</div>
-            </Link>
-
+        <nav className='nav-top'>
+            <div className='title'> BTWN U+I</div>
             <ul>
                 {
                     props.user
-                    ? <li onClick={logout}>Logout</li>
-                    : <li onClick={login}>Login</li>
+                        ? <li className='authent' onClick={logout}>Logout</li>
+                        : <li className='authent' onClick={login}>Login</li>
                 }
 
 
-                
+
             </ul>
         </nav>
     )
-}; 
+};
 export default Header; 
